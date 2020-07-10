099922ec9341e7e590d1981dc4d0d355e639eacd
miily8310s <rillakerika@gmail.com>
2020-07-10 21:54:56 +0900

    add function & class

--- index.js
+++ index.js
@@ -20,3 +20,44 @@ var list = [1, 2, 3];
 var randomValue = 10;
 randomValue = true;
 randomValue = "Vishwas";
+var myVariable = 10;
+var a;
+a = 10;
+a = true;
+var b = 20;
+// not boolean because b's type is number
+// b = true;
+// multiType's type is number or boolean
+var multiType;
+multiType = 20;
+multiType = true;
+var anyType;
+// ?: not need argument
+function add(num1, num2) {
+    return num1 + num2;
+}
+add(5, 10);
+add(4);
+// function fullName(person: { firstName: string; lastName: string }) {
+//   console.log(`My name is ${person.firstName} ${person.lastName}.`);
+// }
+function fullName(person) {
+    console.log("My name is " + person.firstName + " " + person.lastName + ".");
+}
+var p = {
+    firstName: "Bruce",
+    lastName: "Wayne"
+};
+fullName(p);
+var Friend = /** @class */ (function () {
+    function Friend(name) {
+        this.friendName = name;
+    }
+    Friend.prototype.greet = function () {
+        console.log("Hi! " + this.friendName + "!");
+    };
+    return Friend;
+}());
+var fri1 = new Friend("Mary");
+console.log(fri1.friendName);
+fri1.greet();
--- index.ts
+++ index.ts
@@ -26,3 +26,66 @@ let list: number[] = [1, 2, 3];
 let randomValue: any = 10;
 randomValue = true;
 randomValue = "Vishwas";
+
+let myVariable: unknown = 10;
+
+let a;
+a = 10;
+a = true;
+
+let b = 20;
+// not boolean because b's type is number
+// b = true;
+
+// multiType's type is number or boolean
+let multiType: number | boolean;
+multiType = 20;
+multiType = true;
+
+let anyType: any;
+
+// ?: not need argument
+function add(num1: number, num2?: number) {
+  return num1 + num2;
+}
+
+add(5, 10);
+add(4);
+// error codes
+// add(5, "10");
+// add()
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+// function fullName(person: { firstName: string; lastName: string }) {
+//   console.log(`My name is ${person.firstName} ${person.lastName}.`);
+// }
+function fullName(person: Person) {
+  console.log(`My name is ${person.firstName} ${person.lastName}.`);
+}
+
+let p = {
+  firstName: "Bruce",
+  lastName: "Wayne",
+};
+
+fullName(p);
+
+class Friend {
+  friendName: string;
+
+  constructor(name: string) {
+    this.friendName = name;
+  }
+
+  greet() {
+    console.log(`Hi! ${this.friendName}!`);
+  }
+}
+
+let fri1 = new Friend("Mary");
+console.log(fri1.friendName);
+fri1.greet();
